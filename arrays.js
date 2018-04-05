e5a0353e5f04abe6749f12dbd106774fa4f34c98
Spencer Croy <spencerccroy@gmail.com>
2018-04-05 02:02:56 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -31,6 +31,7 @@ function removeElementFromBeginningOfArray(array) {
   return array.slice(1)
 }
 
-function removeElementFromEndOfArray(array) {
-  return array.slice(-1)
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+  return array
 }
