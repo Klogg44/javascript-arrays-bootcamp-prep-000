5743a8273dd1e2189bddd98205367e05eb3fa87c
Spencer Croy <spencerccroy@gmail.com>
2018-04-05 01:53:04 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,8 +4,8 @@ function addElementToBeginningOfArray(array, element) {
   return [element, ...array]
 }
 
-function destructivelyAddElementToBeginningOfArray(array, element) {
-  return array.unshift(element)
+function destructivelyAddElementToBeginningOfArray(arr, element) {
+  return arr.push(element)
 }
 
 function addElementToEndOfArray(array, element) {
