6ecf35b8a90c67e4fd59422c051627397d387961
Spencer Croy <spencerccroy@gmail.com>
2018-04-05 01:24:06 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -15,3 +15,7 @@ function addElementToEndOfArray(array, element) {
 function destructivelyAddElementToEndOfArray(array, element) {
   return array.unshift(element)
 }
+
+function accessElementInArray (array, index) {
+  return array[index]
+}
